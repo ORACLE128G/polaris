95907cbb84211dc92e5791b41e4da4ea1c83485c
oracle128g <oracle128g@gmail.com>
2018-09-17 23:13:39 +0800

    Testing.

--- debugging/calc_test.go
+++ debugging/calc_test.go
@@ -11,13 +11,24 @@ func triangle(a, b int) int {
 func TestTriangle(t *testing.T) {
 	tests := []struct{ a, b, c int }{
 		{3, 4, 5},
-		{6, 7, 8},
-		{3000000, 400000, 700000},
 	}
 	for _, tt := range tests {
 		if actual := triangle(tt.a, tt.b); actual != tt.c {
 			t.Errorf("calcTriangle(%d, %d); got %d, expected %d",
 				tt.a, tt.b, actual, tt.c)
+		}
+	}
+}
+
+
+func BenchmarkTriangle(b *testing.B) {
+	tests := []struct{ a, b, c int }{
+		{3, 4, 5},
+	}
+	for i := 0; i < b.N; i++ {
+		if actual := triangle(tests[0].a, tests[0].b); actual != tests[0].c {
+			b.Errorf("calcTriangle(%d, %d); got %d, expected %d",
+				tests[i].a, tests[i].b, actual, tests[i].c)
 		}
 	}
 }
