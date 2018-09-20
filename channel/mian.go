19a970f98f7e45f5c08708373d334ce3131e3414
oracle128g <oracle128g@gmail.com>
2018-09-20 23:08:00 +0800

    Buffered Channel testing.

--- channel/mian.go
+++ channel/mian.go
@@ -19,22 +19,40 @@ func TryChan() {
 	time.Sleep(time.Microsecond)
 }
 
-func worker(id int, ch chan int) {
-	for {
-		fmt.Printf("Worker %d received %d \n", id, <-ch)
+func createWorker(id int) chan<- int {
+	c := make(chan int)
+	go func() {
+		for {
+			fmt.Printf("Worker %d received %d \n", id, <-c)
+		}
+	}()
+	return c
+}
+func chanDemo() {
+	var channels [10]chan<- int
+	for i := 0; i < 10; i++ {
+		channels[i] = createWorker(i)
 	}
+
+	for i := 0; i < 10; i++ {
+		channels[i] <- 'a' + i
+	}
+	for i := 0; i < 10; i++ {
+		channels[i] <- 'A' + i
+	}
+	time.Sleep(time.Minute)
+}
+
+func makeBufferedChannel() chan int{
+	return make(chan int, 3)
 }
 func main() {
 	// TryChan()
+	// chanDemo()
+	channel := makeBufferedChannel()
+	channel <- 3
+	c := <-channel
+	fmt.Println( c )
 
-	var channels  [10]chan int
-	for i := 0; i < 10; i++ {
-		channels[i] = make(chan int)
-		go worker(i, channels[i])
-	}
 
-	for i := 0; i < 10; i++ {
-		channels[i] <- 'a' +i
-	}
-	time.Sleep(time.Minute)
 }
