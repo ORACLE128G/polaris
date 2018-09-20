ef32e569ad32fac49da4776082e6ffacf6586791
oracle128g <oracle128g@gmail.com>
2018-09-20 22:16:04 +0800

    Go Channel race.

--- channel/mian.go
+++ channel/mian.go
@@ -18,6 +18,18 @@ func TryChan() {
 	c <- 3
 	time.Sleep(time.Microsecond)
 }
+
+func worker(id int, ch chan int) {
+	for {
+		fmt.Printf("Worker %d received %d \n", id, <-ch)
+	}
+}
 func main() {
-	TryChan()
+	// TryChan()
+	ch := make(chan int)
+
+	go worker(1, ch)
+	ch <- 1
+	ch <- 2
+	time.Sleep(time.Minute)
 }
