71b1f951a7fdd68b416204f6a07007184bd4eaaa
oracle128g <oracle128g@gmail.com>
2018-09-23 00:22:30 +0800

    Add new bug feature.

--- oop/eachnodes.go
+++ oop/eachnodes.go
@@ -1,39 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Nodes struct {
-	v           int
-	left, right *Nodes
+	V           int
+	Left, Right *Nodes
 }
 
-func (nodes *Nodes) print () {
-	fmt.Println("Each node is :",nodes.v)
+func (nodes *Nodes) print() {
+	fmt.Println("Each node is :", nodes.V)
 }
 func inst() *Nodes {
 	root := Nodes{0, nil, nil}
-	root.left = &Nodes{1, nil, nil}
-	root.right = &Nodes{2, nil, nil}
-	root.left.left = &Nodes{3, nil, nil}
-	root.left.right = &Nodes{4, nil, nil}
+	root.Left = &Nodes{1, nil, nil}
+	root.Right = &Nodes{2, nil, nil}
+	root.Left.Left = &Nodes{3, nil, nil}
+	root.Left.Right = &Nodes{4, nil, nil}
 
 	return &root
 }
 
-func (nodes *Nodes)setVal(v int) {
-	nodes.left.v = v
+func (nodes *Nodes) setVal(v int) {
+	nodes.Left.V = v
 }
-func (nodes *Nodes)traverse()[]int {
+func (nodes *Nodes) TraverseFunc(f func(*Nodes)) {
 	if nodes == nil {
-		return nil
+		return
 	}
-	nodes.left.traverse()
+	nodes.Left.TraverseFunc(f)
 	nodes.print()
-	nodes.right.traverse()
-	return []int{}
+	nodes.Right.TraverseFunc(f)
+}
+
+func (nodes *Nodes) traverseWithChannel() chan *Nodes {
+	out := make(chan *Nodes)
+	go func() {
+		nodes.TraverseFunc(func(nodes *Nodes) {
+			out <- nodes
+		})
+		close(out)
+	}()
+	return out
 }
 func main() {
 	nodes := inst()
-	nodes.print()
-	nodes.traverse()
+	//nodes.print()
+	//nodes.TraverseFunc()
+
+	maxNode := 0
+	c := nodes.traverseWithChannel()
+	for node := range c {
+		if node.V > maxNode {
+			maxNode = node.V
+		}
+	}
+
+	fmt.Println("Max node value: ", maxNode)
 }
