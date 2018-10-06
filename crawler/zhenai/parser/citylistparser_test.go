9f04594143adc76ab8dbe1d29cb76e726adcfc39
oracle128g <oracle128g@gmail.com>
2018-10-06 09:52:43 +0800

    City list testing.

---
+++ crawler/zhenai/parser/citylistparser_test.go
@@ -0,0 +1,16 @@
+package parser
+
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+)
+
+func TestParseCityList(t *testing.T) {
+	contents, err := ioutil.ReadFile("citylist_test_data.html")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s \n", contents)
+}
