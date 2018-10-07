20bef963e41e1e26cac7448a239f37b3944d0933
oracle128g <oracle128g@gmail.com>
2018-10-07 19:49:00 +0800

    Convertion m[2] to string.

--- crawler/zhenai/parser/citylistparser.go
+++ crawler/zhenai/parser/citylistparser.go
@@ -15,7 +15,7 @@ func ParseCityList(contents [] byte) engine.ParseResult {
 
 	result := engine.ParseResult{}
 	for _, m := range matches {
-		result.Items = append(result.Items, m[2])
+		result.Items = append(result.Items, string(m[2]))
 		result.Requests = append(result.Requests, engine.Request{
 			Url:        string(m[1]),
 			ParserFunc: engine.NilParser,
