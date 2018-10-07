92b286b481f955d2792f67770d7aa1396af11240
oracle128g <oracle128g@gmail.com>
2018-10-07 19:47:53 +0800

    City list parser test pass.

--- crawler/zhenai/parser/citylistparser_test.go
+++ crawler/zhenai/parser/citylistparser_test.go
@@ -1,7 +1,6 @@
 package parser
 
 import (
-	"fmt"
 	"io/ioutil"
 	"testing"
 )
@@ -12,5 +11,38 @@ func TestParseCityList(t *testing.T) {
 		panic(err)
 	}
 
-	fmt.Printf("%s \n", contents)
+	result := ParseCityList(contents)
+
+	const resultSize = 470
+	expectedUrls := [] string{
+		"http://www.zhenai.com/zhenghun/aba",
+		"http://www.zhenai.com/zhenghun/akesu",
+		"http://www.zhenai.com/zhenghun/alashanmeng",
+	}
+
+	expectedCites := [] string{
+		"阿坝", "阿克苏", "阿拉善盟",
+	}
+
+	if len(result.Requests) != resultSize {
+		t.Errorf("result not match, should hava %d requests, but had %d",
+			resultSize, len(result.Requests))
+	}
+
+	for i, url := range expectedUrls {
+		if url != result.Requests[i].Url {
+			t.Errorf("expected url %s, but was %s", url, result.Requests[i].Url)
+		}
+	}
+	if len(result.Items) != resultSize {
+		t.Errorf("result not match, should hava %d items, but had %d",
+			resultSize, len(result.Items))
+	}
+
+	for i, city := range expectedCites {
+		if city != result.Items[i].(string) {
+			t.Errorf("expected city %s, but was %s", city, result.Items[i])
+		}
+	}
+	// fmt.Printf("%s \n", contents)
 }
