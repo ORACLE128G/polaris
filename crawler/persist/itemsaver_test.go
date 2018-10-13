42bfedfe324c6ada5cbacdecf63dd78805a6af93
oracle128g <oracle128g@gmail.com>
2018-10-13 21:54:00 +0800

    Testing Elasticsearch.

--- crawler/persist/itemsaver_test.go
+++ crawler/persist/itemsaver_test.go
@@ -1,6 +1,9 @@
 package persist
 
 import (
+	"context"
+	"encoding/json"
+	"github.com/olivere/elastic"
 	"polaris/crawler/model"
 	"testing"
 )
@@ -11,10 +14,31 @@ func TestSave(t *testing.T) {
 		Age:    18,
 		Height: 175,
 	}
-	id, _ := save(expected)
-	t.Logf("id: %v", id)
-	/*id, err := save(expected)
+	id, err := save(expected)
+
 	if err != nil {
 		panic(err)
-	}*/
+	}
+
+	// Deserialization data by id.
+	client, err := elastic.NewClient(elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+	}
+
+	r , err:= client.Get().
+		Index("polaris").
+		Type("zhenai").
+		Id(id).
+		Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	var actual model.Profile
+	err = json.Unmarshal(*r.Source, &actual)
+	if err != nil {
+		panic(err)
+	}
+	t.Logf("Got source: %+v", actual)
 }
