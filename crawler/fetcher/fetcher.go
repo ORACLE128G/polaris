e21be6f68c575212b8e5951afe5608cf9989408b
oracle128g <oracle128g@gmail.com>
2018-10-28 15:53:39 +0800

    #1 issues fixed.

--- crawler/fetcher/fetcher.go
+++ crawler/fetcher/fetcher.go
@@ -16,7 +16,25 @@ var rateLimiter = time.Tick(30 * time.Millisecond)
 // Fetch data from url
 func Fetch(url string) ([] byte, error) {
 	<-rateLimiter
-	resp, err := http.Get(url)
+
+	// Add request header
+	r, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		panic(err)
+	}
+	r.Header.Add("User-Agent",
+		"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Mobile Safari/537.36")
+	client := http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			fmt.Println("Redirect:", req)
+			return nil
+		},
+	}
+	resp, err := client.Do(r)
+	/*if err != nil{
+		panic(err)
+	}
+	resp, err := http.Get(url)*/
 	if err != nil {
 		return nil, err
 	}
