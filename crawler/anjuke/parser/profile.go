eadd87e996e900cc52b30eaf261709f879046ac3
oracle128g <oracle128g@gmail.com>
2018-11-26 23:31:27 +0800

    Parser of profile.

--- crawler/anjuke/parser/profile.go
+++ crawler/anjuke/parser/profile.go
@@ -2,33 +2,37 @@ package parser
 
 import (
 	"polaris/crawler-distributed/config"
-	"polaris/crawler/engine/anjuke"
+	"polaris/crawler/engine"
 	"polaris/crawler/model"
 	"regexp"
 )
 
 var nameReg = regexp.MustCompile(`<h2>([^<]+)</h2>`)
 var priceReg = regexp.MustCompile(`<em class="sp-price other-price">([^<]+)</em>`)
-var buildYearsReg = regexp.MustCompile(`<em class="sp-price other-price">([^<]+)</em>`)
+var buildYearsReg = regexp.MustCompile(`<dd><span>([^<]+)</span></dd>`)
 var idReg = regexp.MustCompile(`data-loupan_id="([^"]+)"`)
+var addressReg = regexp.MustCompile(`<a class="lpAddr-text g-overflow" href="([.]+)" 
+soj="loupan_index_map" target="_blank">(^<)+</a>`)
 
-const types = "anjuke"
+var imgReg = regexp.MustCompile(`<img width="([^"]+)" height="([^"]+)" src="(https://pic4.ajkimg.com/display/[^"]+)">`)
 
-func parseProfile(contents []byte, url string) anjuke.ParseResult {
+func parseProfile(contents []byte, url string) engine.ParseResult {
 	profile := model.AnjukeProfile{}
 
 	profile.Id = extractString(contents, idReg)
 	profile.Name = extractString(contents, nameReg)
 	profile.Price = extractString(contents, priceReg)
 	profile.BuildYears = extractString(contents, buildYearsReg)
-
-	result := anjuke.ParseResult{
-		Items: []anjuke.Item{
+	profile.Address = extractString(contents, addressReg)
+	profile.Images = extractImages(contents, imgReg)
+	result := engine.ParseResult{
+		Items: []engine.Item{
 			{
-				Url:     url,
-				Type:    types,
-				Id:      profile.Id,
-				PayLoad: profile,
+				Url:    url,
+				// todo: anjuke
+				Type:   "zhenai",
+				Id:     profile.Id,
+				Anjuke: profile,
 			},
 		},
 	}
@@ -43,6 +47,16 @@ func extractString(b []byte, r *regexp.Regexp) string {
 	return ""
 }
 
+func extractImages(b []byte, reg *regexp.Regexp) []string {
+
+	match := reg.FindAllSubmatch(b, -1)
+	var r []string
+	for _, e := range match {
+		r = append(r, string(e[3]))
+	}
+	return r
+}
+
 type ProfileParser struct {
 }
 
@@ -52,7 +66,7 @@ func (p *ProfileParser) Serialize() (name string, args interface{}) {
 }
 
 // Parse profile
-func (p *ProfileParser) Parse(contents []byte, url string) anjuke.ParseResult {
+func (p *ProfileParser) Parse(contents []byte, url string) engine.ParseResult {
 
 	return parseProfile(contents, url)
 }
