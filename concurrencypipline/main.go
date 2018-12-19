b6ebddaf8c855a32ad4a2d8954e405c48b5e918c
oracle128g <oracle128g@gmail.com>
2018-12-19 22:49:20 +0800

    Add more nodes.

--- concurrencypipline/main.go
+++ concurrencypipline/main.go
@@ -1,11 +1,49 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"polaris/concurrencypipline/pipline"
 )
 
+const (
+	in   = "large.in"
+	size = 100000000
+)
+
 func main() {
+	file, err := os.Create(in)
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+	p := pipline.RandomSource(size)
+
+	writer := bufio.NewWriter(file)
+	pipline.WriteSink(writer, p)
+	writer.Flush()
+
+	file, err = os.Open(in)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	p = pipline.ReaderSource(bufio.NewReader(file))
+
+	count := 0
+	for v := range p {
+		fmt.Println(v)
+		count++
+		if count == 100 {
+			break
+		}
+	}
+}
+
+func Merge() {
 	out := pipline.Merge(
 		pipline.InMemSort(pipline.ArraySource(1, 5, 2, 10, 6, 3)),
 		pipline.InMemSort(pipline.ArraySource(3, 4, 9, 0, 11, 8)),
--- concurrencypipline/pipline/nodes.go
+++ concurrencypipline/pipline/nodes.go
@@ -1,6 +1,11 @@
 package pipline
 
-import "sort"
+import (
+	"encoding/binary"
+	"io"
+	"math/rand"
+	"sort"
+)
 
 func ArraySource(ints ... int) <-chan int {
 
@@ -53,5 +58,47 @@ func Merge(in1, in2 <-chan int) <-chan int {
 		close(out)
 	}()
 	return out
+}
 
+func ReaderSource(reader io.Reader) <-chan int {
+	out := make(chan int)
+	go func() {
+		// 1byte = 8bit
+		// 1int = 8byte = 64bit
+		b := make([] byte, 8)
+
+		for {
+			n, err := reader.Read(b)
+			if n > 0 {
+				// transform b to Uint64
+				v := int(binary.BigEndian.Uint64(b))
+				out <- v
+			}
+
+			if err != nil {
+				break
+			}
+		}
+		close(out)
+	}()
+	return out
+}
+
+func WriteSink(write io.Writer, in <-chan int) {
+	for v := range in {
+		buf := make([]byte, 8)
+		binary.BigEndian.PutUint64(buf, uint64(v))
+		write.Write(buf)
+	}
+}
+
+func RandomSource(count int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for i := 0; i < count; i++ {
+			out <- rand.Int()
+		}
+		close(out)
+	}()
+	return out
 }
