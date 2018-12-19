ebc2b88332f6d3ac11038cf063878dd17b1eff3d
oracle128g <oracle128g@gmail.com>
2018-12-19 22:03:42 +0800

    Go pipline nodes.

--- concurrencypipline/main.go
+++ concurrencypipline/main.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
-	out := pipline.InMemSort(pipline.ArraySource(1, 5, 2, 10, 6, 3))
+	out := pipline.Merge(
+		pipline.InMemSort(pipline.ArraySource(1, 5, 2, 10, 6, 3)),
+		pipline.InMemSort(pipline.ArraySource(3, 4, 9, 0, 11, 8)),
+	)
+
 	for v := range out {
 		fmt.Println(v)
 	}
--- concurrencypipline/pipline/nodes.go
+++ concurrencypipline/pipline/nodes.go
@@ -16,26 +16,41 @@ func ArraySource(ints ... int) <-chan int {
 }
 
 func InMemSort(in <-chan int) <-chan int {
-
 	out := make(chan int)
-
 	go func() {
 		var all []int
-
 		for v := range in {
 			all = append(all, v)
 		}
-
 		// Sort
 		sort.Ints(all)
-
 		// Output
 		for _, v := range all {
 			out <- v
 		}
-
 		close(out)
+	}()
+	return out
+}
 
+func Merge(in1, in2 <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		v1, ok1 := <-in1
+		v2, ok2 := <-in2
+
+		for ok1 || ok2 {
+			// v1 has value
+			if !ok2 || (ok1 && v1 <= v2) {
+				out <- v1
+				// continue take out a next value
+				v1, ok1 = <-in1
+			} else {
+				out <- v2
+				v2, ok2 = <-in2
+			}
+		}
+		close(out)
 	}()
 	return out
 
