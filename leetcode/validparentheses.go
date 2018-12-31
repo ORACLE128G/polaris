e93f3290de57ca7132858582db049e350059efd7
oracle128g <oracle128g@gmail.com>
2018-12-31 21:09:48 +0800

    Valid parentheses.

---
+++ leetcode/validparentheses.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Valid Parentheses
+// see: https://leetcode.com/problems/valid-parentheses/
+func main() {
+	fmt.Println(isValid("(([][]))"))
+}
+
+
+func isValid(s string) bool {
+	size := len(s)
+
+	stack := make([]byte, size)
+	top := 0
+
+	for i := 0; i < size; i++ {
+		c := s[i]
+		switch c {
+		case '(':
+			stack[top] = c + 1 // '('+1 is ')'
+			top++
+		case '[', '{':
+			stack[top] = c + 2
+			top++
+		case ')', ']', '}':
+			// if 's' like "(([][]))"
+			// stack must be like "))]]"
+			if top > 0 && stack[top-1] == c {
+				top--
+			} else {
+				return false
+			}
+		}
+	}
+
+	return top == 0
+}
